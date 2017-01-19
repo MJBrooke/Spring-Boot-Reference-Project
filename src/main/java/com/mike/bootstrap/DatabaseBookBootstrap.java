8c4eb8a89c0cfaeca03bcd1d9775769961f4e2a1
michael86 <michaelb5@discovery.co.za>
2017-01-19 17:53:39 +0200

    Small changes to the REST controller and extra comments

--- src/main/java/com/mike/bootstrap/DatabaseBookBootstrap.java
+++ src/main/java/com/mike/bootstrap/DatabaseBookBootstrap.java
@@ -7,6 +7,14 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+/*
+This component catches the startup of the Spring Context.
+We can use this hook to load up runtime dev database with data through the use of our services.
+
+An advantage of this approach is that the component can be profiled easily, as it is simply a bean.
+The disadvantage is that we are not using native SQL, which makes this a little less maintainable with changes.
+ */
+
 @Component
 public class DatabaseBookBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
--- src/main/java/com/mike/controller/rest/BookRestController.java
+++ src/main/java/com/mike/controller/rest/BookRestController.java
@@ -5,6 +5,7 @@ import com.mike.service.BookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ Here is a simple example of a REST controller, which exposes its contents as JSO
  */
 
 @RestController
-@RequestMapping("/rest")
+@RequestMapping("/rest/book")
 public class BookRestController {
 
     //How to get an instance of the logger for proper logging
@@ -24,14 +25,19 @@ public class BookRestController {
     @Autowired
     private BookService bookService;
 
-    @RequestMapping("/book")
-    public Book getBook() {
+    @RequestMapping("/{id}")
+    public Book getBook(@PathVariable Integer id) {
+        return bookService.getBookById(id);
+    }
+
+    @RequestMapping("/list")
+    public List<Book> getBookList() {
 
         //Example of the different levels of logging, which can be turned on or off in the application.properties file.
         logger.info("[INFO] getBook() REST service hit");
         logger.debug("[DEBUG] getBook() REST service hit");
         logger.error("[ERROR] getBook() REST service hit");
 
-        return bookService.getBookById(1);
+        return bookService.getBookList();
     }
 }
